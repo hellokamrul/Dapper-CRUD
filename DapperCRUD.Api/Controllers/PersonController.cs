857bd834a2d30071dc7c75a784a00b8604b0b29d
unknown <nijumkamrul447@gmail.com>
2024-11-01 16:10:51 +0600

    Get List
    -Import Dapper Package
    -Import SqlClient
    -Config IConfiguration
    -Database COnnection string

---
+++ DapperCRUD.Api/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCRUD.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public PersonController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Persons>>> GetAllPersonList()
+        {
+            using var conncetion = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var persons = await conncetion.QueryAsync<Persons>("select * from Person");
+            return Ok(persons);
+        }
+
+    }
+}
